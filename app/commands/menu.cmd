558ee6f7b16a44dc4b496c22c24ff60671d78e61
Courtenay Probert <courtenay@probert.me.uk>
2013-07-09 19:58:16 +0100

    open scms in website dir

--- app/commands/menu.cmd
+++ app/commands/menu.cmd
@@ -2,6 +2,7 @@
 cd /d %0\..
 set "WEBSITES=%USERPROFILE%\Documents\websites"
 IF NOT EXIST %WEBSITES% md %WEBSITES%
+cd /D %WEBSITES%
 echo Opening Command Window
 start cmd /K call "%cd%\menu-run.cmd"
 exit
